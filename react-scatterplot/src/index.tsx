af8e34d14da78d6800e495d13dfd4267ffa2bca4
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-09-26 17:01:22 -0300

    Finish typescript config

--- react-scatterplot/src/index.tsx
+++ react-scatterplot/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom"
 
 import Hello from './components/Hello'
 
--- react-scatterplot/webpack.config.js
+++ react-scatterplot/webpack.config.js
@@ -23,10 +23,8 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    plugins: [
-        new webpack.ProvidePlugin({
-          "React": "react",
-          "ReactDOM": "react-dom"
-        }),
-    ],
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
 };
