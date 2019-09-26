b4994ba0d8153559e07d7a46255d91d0fe74046c
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-09-26 15:15:51 -0300

    Init scatterplot package

---
+++ react-scatterplot/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, '/src/index.tsx'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    plugins: [
+        new webpack.ProvidePlugin({
+          "React": "react",
+          "ReactDOM": "react-dom"
+        }),
+    ],
+};
